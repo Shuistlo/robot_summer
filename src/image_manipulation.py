ffa9f8d2f8cfeb51b5f341ef4f17553312e72430
Shu <k1630593@kcl.ac.uk>
2019-07-21 11:02:27 +0100

    made the image manipulation file actually usable for colour filtering

--- src/image_manipulation.py
+++ src/image_manipulation.py
@@ -8,5 +8,9 @@ from cv_bridge import CvBridge, CvBridgeError
 # OpenCV2 for saving an image
 import cv2
 
-def colourFilter(image):
-
+#lower and upper bounds are of a colour you want to filter in hsv
+#follow https://toolstud.io/color/rgb.php to convert a colour into hsv
+def colourFilter(image, lowerBound, upperBound):
+	hsv_image = cv2.cvtColor(nemo, cv2.COLOR_RGB2HSV)
+	mask = cv2.inRange(hsv_image, lowerBound, upperBound)
+	return cv2.bitwise_and(image, image, mask=mask)
